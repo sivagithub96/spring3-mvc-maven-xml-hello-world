9687aee136b45d3fad4ee94381b7ced6349ade28
sivagithub96 <80754705+sivagithub96@users.noreply.github.com>
2021-03-26 14:49:24 +0530

    Update HelloController.java

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String printdaffa venkat(ModelMap model) {
+	public String printdaffavenkat(ModelMap model) {
 
 		model.addAttribute("message", "Spring 3 MVC Hello World");
 		return "hello";
